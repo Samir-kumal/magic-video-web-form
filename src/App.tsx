6cd4a9502c71593aeccede7c685b52986a6cca1c
Aman Shakya <amanshakya9912@gmail.com>
2024-05-07 19:25:04 +0545

    create magic video functionality

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,13 @@ import { Button } from "./components/ui/button";
 import useDataProvider from "./hooks/useDataProvider";
 import { PageAction } from "./context/DataContext";
 import ProgressNumHeader from "./components/common/ProgressNumHeader";
-
-
+import "./App.css";
 
 const App = () => {
   const [progress, setProgress] = useState(25);
   const { updatePage, page } = useDataProvider();
 
-
-
   const handleNextPage = () => {
-    
     setProgress((oldProgress) => {
       if (oldProgress === 100) {
         return 25;
@@ -30,7 +26,6 @@ const App = () => {
       return oldProgress + 25;
     });
     updatePage(PageAction.NEXT);
-
   };
 
   const handlePreviousPage = () => {
@@ -41,16 +36,16 @@ const App = () => {
       return oldProgress - 25;
     });
     updatePage(PageAction.PREVIOUS);
-  }
+  };
 
-  console.log("page no is ", page)
+  console.log("page no is ", page);
   return (
     <div className="w-screen min-h-screen max-h-fit bg-slate-300">
       <section className="w-screen h-fit flex flex-col items-center justify-center">
         <div className="py-6">
           <h1 className="text-2xl font-bold">Magic Video Web App</h1>
         </div>
-       <ProgressNumHeader progress={progress} />
+        <ProgressNumHeader progress={progress} />
         <CardWrapper>
           {/* <CardHeader>
           
@@ -62,15 +57,19 @@ const App = () => {
             <CardTitle>Step 1 - Insert the name and a video of the subject you want to train</CardTitle>
           </CardHeader> */}
           {/* <CardContent> */}
-            <Form />
+          <Form />
           {/* </CardContent> */}
           <CardContent className="flex justify-end gap-x-3">
-           {page !==1 && <Button type="button" onClick={handlePreviousPage}>
-              Previous
-            </Button>}
-           {page !==4 && <Button type="button" onClick={handleNextPage}>
-              Next
-            </Button>}
+            {page !== 1 && (
+              <Button type="button" onClick={handlePreviousPage}>
+                Previous
+              </Button>
+            )}
+            {page !== 4 && (
+              <Button type="button" onClick={handleNextPage}>
+                Next
+              </Button>
+            )}
           </CardContent>
         </CardWrapper>
       </section>
--- src/components/form/Form4.tsx
+++ src/components/form/Form4.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import ReactPlayer from "react-player";
 import { Form, useForm } from "react-hook-form";
 import { Button } from "../ui/button";
+import axios from "axios";
+import { BASE_URL } from "@/context/DataContext";
+import { Progress } from "@radix-ui/react-progress";
 
 type FormInputs = {
   duration: number;
   marked_time: number;
 };
+
 const Form4 = () => {
   const playerRef = useRef(null);
   const [markedTime, setMarkedTime] = useState<number>(0);
@@ -20,19 +24,75 @@ const Form4 = () => {
     setValue,
     formState: { errors },
   } = useForm<FormInputs>();
+  const [progress, setProgress] = useState({
+    isStarted: false,
+    value: 0,
+    isFailed: false,
+  });
+  const [uploadMsg, setUploadMsg] = useState("");
+  const [url, setUrl] = useState(false);
+  const onSubmit = async (data: any) => {
+    // console.log("Form data:", data);
+    // console.log("Marked Time:", data.marked_time);
+    // console.log("ft,", formattedTime);
+    data.marked_time = formattedTime;
+    console.log("new data", data);
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/api/get_magic_video`,
+        data,
 
-  const onSubmit = (data: any) => {
-    console.log("Form data:", data);
-    console.log("Marked Time:", data.marked_time);
-    console.log("Marked Time actual:", markedTime);
+        {
+          onUploadProgress: (progressEvent) => {
+            if (progressEvent && progressEvent.progress) {
+              setUploadMsg("Creating Magic Video..");
+              console.log(
+                "Upload Progress: " +
+                  Math.round(progressEvent.progress * 100) +
+                  "%"
+              );
+              setProgress((previousState) => ({
+                ...previousState,
+                isStarted: true,
+                isFailed: false,
+                value: Math.round(progressEvent.progress * 100),
+              }));
+            }
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      // if (response.data) {
+      //   console.log(response.data.status);
+      //   // setUploadMsg("Magic Video Created Successfully");
+      // }
+      console.log(response.data.success);
+      // console.log(response.data.url);
+      setUrl(true);
+      setUploadMsg(response.data.success);
+    } catch (err: any) {
+      if (err.response && err.response.data && err.response.data.errors) {
+        console.log(err);
+        setUploadMsg("File upload failed");
+        setProgress((previousState) => ({
+          ...previousState,
+          isFailed: true,
+        }));
+      }
+    }
   };
+
   const handlePause = () => {
     if (playerRef.current) {
       const playerInstance = playerRef.current as ReactPlayer;
       const currentTime = playerInstance.getCurrentTime();
       console.log(`Video paused at ${currentTime} seconds`);
-      setMarkedTime(Number(currentTime));
-      setValue("marked_time", Number(currentTime));
+      const roundedTime = parseFloat(currentTime.toFixed(0));
+      setMarkedTime(Number(roundedTime));
+      setValue("marked_time", Number(roundedTime));
     }
   };
   const handleDuration = (duration: any) => {
@@ -51,13 +111,48 @@ const Form4 = () => {
     setValidDuration(inputDuration + (markedTime ?? 0) > videoDuration);
     console.log(validDuration);
   }, [inputDuration, markedTime, videoDuration]);
+
+  const dateString = "2024-04-17 10:00:00";
+
+  // Create a Date object from the string
+  const dateObject = new Date(dateString);
+
+  // Extract hours, minutes, and seconds
+  const hours = dateObject.getHours();
+  var minutes = dateObject.getMinutes();
+  const seconds = dateObject.getSeconds();
+  // const givenTimeMilliseconds = markedTime * 100;
+  // console.log("ml", givenTimeMilliseconds);
+  var newSeconds = seconds + markedTime;
+
+  // Check if adding 3 seconds causes the minutes to increment
+  if (newSeconds >= 60) {
+    // Increment minutes and reset seconds
+    minutes++;
+    newSeconds -= 60; // Subtract 60 to get the remaining seconds after incrementing minutes
+  }
+
+  // Update the seconds in the Date object
+  // baseTime.setSeconds(newSeconds);
+
+  // Format the updated time
+  const formattedTime = `2024-04-17 ${hours
+    .toString()
+    .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${newSeconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  console.log("ft", formattedTime);
   return (
     <>
+      <CardHeader>
+        <CardTitle>Step 4 - Create magic video</CardTitle>
+      </CardHeader>
       <Card className="flex items-center justify-center border-0 shadow-none">
         <CardContent className="gap-2">
           <ReactPlayer
             ref={playerRef}
-            url={"src/assets/mrbean(cliped).mp4"}
+            url={"src/assets/bean2.mp4"}
             width={"100%"}
             height={"100%"}
             onPause={handlePause}
@@ -71,9 +166,11 @@ const Form4 = () => {
                 className="p-2 border rounded-md mt-1 appearance-none"
                 id="marked_time"
                 type="number"
-                step={0.000001}
                 {...register("marked_time", {
-                  required: true,
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
                   validate: {
                     required: (value) => {
                       if (value > videoDuration)
@@ -99,10 +196,12 @@ const Form4 = () => {
                 className="p-2 border rounded-md mt-1 appearance-none"
                 id="duration"
                 type="number"
-                step={0.000001}
                 onInput={handleInputChange}
                 {...register("duration", {
-                  required: true,
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
                   validate: {
                     required: (value) => {
                       if (validDuration)
@@ -123,6 +222,28 @@ const Form4 = () => {
             <Button type="submit" className="mt-5 float-end">
               Create Magic Video
             </Button>
+            {/* <p className="text-[10px]">{uploadMsg}</p> */}
+            {progress.isStarted && (
+              <section>
+                <Progress
+                  value={progress.value}
+                  className="w-40 h-[3px] "
+                  classNameCustom={
+                    progress.isFailed ? "bg-red-500" : `bg-green-500`
+                  }
+                />
+                <p className="text-[12px]">{uploadMsg}</p>
+              </section>
+            )}
+            {url && (
+              <>
+                <Button className="mt-1">
+                  <a href="http://192.168.1.151:5001/api/magic_video_download">
+                    Download Video
+                  </a>
+                </Button>
+              </>
+            )}
           </form>
         </CardContent>
       </Card>
